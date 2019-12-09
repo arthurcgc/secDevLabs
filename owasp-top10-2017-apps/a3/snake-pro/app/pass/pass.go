9e1f3034a4638f02a43eaa207af7a716f2a913f6
arthurcgc <arthur.cavalcante.puc@gmail.com>
2019-12-09 12:24:26 -0300

    trying to resolve import errors

--- owasp-top10-2017-apps/a3/snake-pro/app/pass/pass.go
+++ owasp-top10-2017-apps/a3/snake-pro/app/pass/pass.go
@@ -1,6 +1,25 @@
 package pass
 
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 // CheckPass checks a password
 func CheckPass(truePassword, attemptPassword string) bool {
-	return truePassword == attemptPassword
+	return comparePasswords(truePassword, []byte(attemptPassword))
+}
+
+func comparePasswords(hashedPwd string, plainPwd []byte) bool {
+	// Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
 }
